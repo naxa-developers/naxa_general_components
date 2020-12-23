3abae0fc6ff61186c5cb36b6eeaa003b8ea5976b
Binabh <binabhdevkota@gmail.com>
2020-12-23 06:34:21 +0545

    dropdown done

--- src/components/Dropdown/dropdown.stories.js
+++ src/components/Dropdown/dropdown.stories.js
@@ -1,40 +1,121 @@
-import React from 'react';
-import Dropdown from './index.js';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import React from "react";
+import Dropdown from "./index.js";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
 
-storiesOf('Dropdown', module)
+storiesOf("Dropdown", module)
   .addDecorator(withKnobs)
-  .add('Default', () => (
-    <div className="custom-dropdown active">
-        <button className="common-button is-bg">
+  .add("Click and hover", () => (
+    <Dropdown
+      trigger={(toggleContent) => (
+        <button className="common-button is-bg" onClick={toggleContent}>
           <span>Dropdown</span>
           <i class="fas fa-caret-down"></i>
         </button>
-        <ul class="custom-dropdown-menu">
-            <li><a>List1</a></li>
-            <li><a>List2</a></li>
-            <li><a>List3</a></li>
-            <li><a>List4</a></li>
-            <li><a>List5</a></li>
-            <li><a>List6</a></li>
-        </ul>
-    </div>
+      )}
+      content={() => (
+        <>
+          <li>
+            <a>List1</a>
+          </li>
+          <li>
+            <a>List2</a>
+          </li>
+          <li>
+            <a>List3</a>
+          </li>
+          <li>
+            <a>List4</a>
+          </li>
+          <li>
+            <a>List5</a>
+          </li>
+          <li>
+            <a>List6</a>
+          </li>
+        </>
+      )}
+    />
   ))
-  .add('with icons', () => (
-    <div className="custom-dropdown">
-        <button className="common-button is-bg">
-        <span>Dropdown</span>
+  .add("Hover only", () => (
+    <Dropdown
+      trigger={(toggleContent) => (
+        <button className="common-button is-bg" onClick={() => {}}>
+          <span>Dropdown</span>
           <i class="fas fa-caret-down"></i>
         </button>
-        <ul class="custom-dropdown-menu">
-            <li><a><i class="fas fa-fan"></i>List1</a></li>
-            <li><a><i class="fas fa-tree"></i>List2</a></li>
-            <li><a><i class="fab fa-pagelines"></i>List3</a></li>
-            <li><a><i class="fas fa-home"></i>List4</a></li>
-            <li><a><i class="fas fa-globe"></i>List5</a></li>
-            <li><a><i class="fas fa-hand-holding-water"></i>List6</a></li>
-        </ul>
-    </div>
+      )}
+      content={() => (
+        <>
+          <li>
+            <a>List1</a>
+          </li>
+          <li>
+            <a>List2</a>
+          </li>
+          <li>
+            <a>List3</a>
+          </li>
+          <li>
+            <a>List4</a>
+          </li>
+          <li>
+            <a>List5</a>
+          </li>
+          <li>
+            <a>List6</a>
+          </li>
+        </>
+      )}
+    />
   ))
+  .add("Click only", () => (
+    <Dropdown
+      trigger={(toggleContent) => (
+        <button className="common-button is-bg" onClick={toggleContent}>
+          <span>Dropdown</span>
+        </button>
+      )}
+      content={(showContent) => {
+        if (showContent) {
+          return (
+            <>
+              <li>
+                <a>
+                  <i class="fas fa-fan"></i>List1
+                </a>
+              </li>
+              <li>
+                <a>
+                  <i class="fas fa-tree"></i>List2
+                </a>
+              </li>
+              <li>
+                <a>
+                  <i class="fab fa-pagelines"></i>List3
+                </a>
+              </li>
+              <li>
+                <a>
+                  <i class="fas fa-home"></i>List4
+                </a>
+              </li>
+              <li>
+                <a>
+                  <i class="fas fa-globe"></i>List5
+                </a>
+              </li>
+              <li>
+                <a>
+                  <i class="fas fa-hand-holding-water"></i>List6
+                </a>
+              </li>
+            </>
+          );
+        } else {
+          return null;
+        }
+      }}
+    />
+  ));
--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import './dropdown.scss';
-import '../Button/button.scss';
+import React, { useState } from "react";
+import "./dropdown.scss";
+import "../Button/button.scss";
+import PropTypes from "prop-types";
 
-const Dropdown = (props) => (
-  <div className={`custom-dropdown ${props.className}`} onClick={props.onClick}>
-     {<span>{props.label}</span>}
-  </div>
-);
+function Dropdown({ className, trigger, content }) {
+  const [showContent, setShowContent] = useState(false);
+  function toggleContent(e) {
+    e.stopPropagation();
+    setShowContent(!showContent);
+  }
+
+  function closeContent(e) {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setShowContent(false);
+    }
+  }
+
+  return (
+    <div
+      className={`custom-dropdown ${className} ${showContent ? "active" : ""}`}
+      onBlur={closeContent}
+      tabIndex="0"
+      role="button"
+    >
+      {trigger(toggleContent)}
+      <ul class={`custom-dropdown-menu`}>{content(showContent)}</ul>
+    </div>
+  );
+}
+
+Dropdown.propTypes = {
+  className: PropTypes.string,
+  trigger: PropTypes.func.isRequired,
+  content: PropTypes.func.isRequired,
+};
+
+Dropdown.defaultProps = {
+  className: "",
+};
+
 export default Dropdown;
