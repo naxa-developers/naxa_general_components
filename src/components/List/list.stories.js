83d0c6858fcc24d73872d6fbf8a42f9e8db7162a
royallsilwallz <royalsilwal@gmail.com>
2021-01-06 17:42:20 +0545

    list component added

--- src/components/List/list.stories.js
+++ src/components/List/list.stories.js
@@ -1,51 +1,48 @@
 import React from 'react';
-import List from './index.js';
+import List, { ListItem } from './index.js';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import Duolingo from './../../img/tabs/Duolingo.png';
 import Flurry from './../../img/tabs/Flurry.png';
+import { list, listWithLinks } from './list.data';
 
 storiesOf('Lists', module)
   .addDecorator(withKnobs)
-  .add('Default', () => ( 
-      <ul class="list is-icons">
-        <li><i class="fas fa-angle-double-right"></i>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam perspiciatis unde omnis iste natus error  totam rem aperiam  perspiciatis unde omnis iste natus error </li>
-        <li><i class="fas fa-angle-double-right"></i>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</li>
-        <li><i class="fas fa-angle-double-right"></i>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperia</li>
-        <li><i class="fas fa-angle-double-right"></i>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</li>
-      </ul>
+  .add('Default', () => (
+    <List className='is-icons'>
+      {list.map(item => (
+        <ListItem>{item.title}</ListItem>
+      ))}
+    </List>
   ))
-  
-  .add('With Links', () => ( 
-    <ul class="list is-icons">
-      <li><i class="fas fa-angle-double-right"></i><a href="#">Sed ut perspiciatis unde omnis iste</a> </li>
-      <li><i class="fas fa-angle-double-right"></i><a href="#">omnis iste natus error </a></li>
-      <li><i class="fas fa-angle-double-right"></i><a href="#">Et harum quidem rerum facilis</a></li>
-      <li><i class="fas fa-angle-double-right"></i><a href="#">ut perspiciatis unde omnis</a></li>
-    </ul>
-))
-  .add('With Image', () => ( 
-    <ul class="list tab-list">
-      <li>
+
+  .add('With Links', () => (
+    <List className='list is-icons'>
+      {listWithLinks.map(item => (
+        <ListItem withLink link='#'>
+          {item.title}
+        </ListItem>
+      ))}
+    </List>
+  ))
+  .add('With Image', () => (
+    <List class='list tab-list'>
+      <ListItem
+        withImage
+        imageTag={
           <a>
-              <img src={Duolingo} alt="image"/>
+            <img src={Duolingo} alt='image' />
           </a>
-      </li>
-      <li>
+        }
+      ></ListItem>
+      <ListItem
+        withImage
+        imageTag={
           <a>
-              <img src={Flurry} alt="image"/>
+            <img src={Flurry} alt='image' />
           </a>
-      </li>
-      <li>
-          <a>
-              <img src={Duolingo} alt="image"/>
-          </a>
-      </li>
-      <li>
-          <a>
-              <img src={Flurry} alt="image"/>
-          </a>
-      </li>
-    </ul>
-))
+        }
+      ></ListItem>
+    </List>
+  ));
