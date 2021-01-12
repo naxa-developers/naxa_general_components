dc3ebf30098eb6001eed84979e12b2c0c118a57b
srthapa <srthapa_611@yahoo.com>
2021-01-12 21:08:56 +0545

    Resolve conflict

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,146 +1,82 @@
-<<<<<<< HEAD
-import React, { Component, Children } from 'react';
-import PropTypes from 'prop-types';
-=======
 import React, { useState } from 'react';
 import Duolingo from './../../img/tabs/Duolingo.png';
 import Flurry from './../../img/tabs/Flurry.png';
->>>>>>> a927914cb247d4067616864c033478c6d55be8e0
 import './tabs.scss';
 
-<<<<<<< HEAD
-function Tab({ tab, children }) {
-  return <div tab={tab}>{children}</div>;
-}
-
-class Tabs extends Component {
-  constructor(props) {
-    super(props);
-    const {
-      props: { children },
-    } = this;
-    this.state = {
-      activeTab:
-        children && Children.count(children) === 1
-          ? children.props.tab
-          : children[0].props.tab,
-    };
-  }
-
-  onClickTabItem = (tab) => {
-    const { getActiveTab } = this.props;
-    this.setState({ activeTab: tab });
-    getActiveTab(tab);
-  };
-
-  render() {
-    const {
-      onClickTabItem,
-      props: { children, className },
-      state: { activeTab },
-    } = this;
-
-    return (
-      <div className="custom-tab">
-        <ul className={`tab-list is-bg ${className}`}>
-          {Children.map(children, (child) => {
-            const { tab } = child.props;
-            return (
-              <li key={tab} className={activeTab === tab ? 'current' : ''}>
-                {/* eslint-disable-next-line */}
-                <a
-                  onClick={() => onClickTabItem(tab)}
-                  onKeyDown={() => onClickTabItem(tab)}
-                  role="button"
-                  tabIndex={0}
-                  style={{ outline: 0 }}
-                >
-                  {tab}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <div className="custom-tab-content">
-          {Children.map(children, (child) => {
-            if (child.props.tab !== activeTab) return null;
-            return child.props.children;
-          })}
+function Tabs({ tabClassName }) {
+  const [tabState, setTabState] = useState(0);
+  return (
+    <div className="custom-tab">
+      <ul
+        className={
+          tabClassName == 'tab-list is-center'
+            ? 'tab-list is-bg is-center'
+            : tabClassName
+        }
+      >
+        <li
+          className={tabState === 0 ? 'current' : ''}
+          onClick={() => setTabState(0)}
+        >
+          <a data-tab="tab1">Test 1</a>
+        </li>
+        <li
+          className={tabState === 1 ? 'current' : ''}
+          onClick={() => setTabState(1)}
+        >
+          <a data-tab="tab2">Test 2</a>
+        </li>
+        <li
+          className={tabState === 2 ? 'current' : ''}
+          onClick={() => setTabState(2)}
+        >
+          <a data-tab="tab3">Test 3</a>
+        </li>
+      </ul>
+      <div className="custom-tab-content">
+        <div className="tab-item" id="tab1">
+          <ul
+            className={
+              tabClassName == 'tab-list is-center'
+                ? 'tab-list is-bg is-center'
+                : 'tab-list'
+            }
+          >
+            <li>
+              <a href="#">
+                <img src={Duolingo} alt="client" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src={Flurry} alt="client" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="tab-item current" id="tab2">
+          <ul
+            className={
+              tabClassName == 'tab-list is-center'
+                ? 'tab-list is-bg is-center'
+                : 'tab-list'
+            }
+          >
+            <li>
+              <a href="#">
+                <img src={Duolingo} alt="client" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src={Flurry} alt="client" />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-    );
-  }
-}
-
-Tab.propTypes = {
-  tab: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-};
-
-Tabs.defaultProps = {
-  header: null,
-  className: '',
-  footer: null,
-  getActiveTab: () => {},
-};
-
-Tabs.propTypes = {
-  children: PropTypes.node.isRequired,
-  className: PropTypes.string,
-  header: PropTypes.node,
-  getActiveTab: PropTypes.func,
-  footer: PropTypes.node,
-};
-
-export { Tab };
-export default Tabs;
-=======
-function Tabs({ tabClassName }) {
-    const [tabState, setTabState] = useState(0);
-    return (
-
-        <div className="custom-tab">
-            <ul className={tabClassName == 'tab-list is-center' ? "tab-list is-bg is-center" : tabClassName}>
-                <li className={tabState === 0 ? "current" : ""} onClick={() => setTabState(0)}><a data-tab="tab1">Test 1</a></li>
-                <li className={tabState === 1 ? "current" : ""} onClick={() => setTabState(1)}><a data-tab="tab2">Test 2</a></li>
-                <li className={tabState === 2 ? "current" : ""} onClick={() => setTabState(2)}><a data-tab="tab3">Test 3</a></li>
-            </ul>
-            <div className="custom-tab-content">
-                <div className="tab-item" id="tab1">
-                    <ul className={tabClassName == 'tab-list is-center' ? "tab-list is-bg is-center" : "tab-list"}>
-
-                        <li>
-                            <a href="#">
-                                <img src={Duolingo} alt="client" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img src={Flurry} alt="client" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="tab-item current" id="tab2">
-                    <ul className={tabClassName == 'tab-list is-center' ? "tab-list is-bg is-center" : "tab-list"}>
-                        <li>
-                            <a href="#">
-                                <img src={Duolingo} alt="client" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img src={Flurry} alt="client" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-
-    )
+    </div>
+  );
 }
 
 export default Tabs;
->>>>>>> a927914cb247d4067616864c033478c6d55be8e0
--- src/components/Tabs/tab.stories.js
+++ src/components/Tabs/tab.stories.js
@@ -2,321 +2,81 @@ import React from 'react';
 import Tabs, { Tab } from './index.js';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-<<<<<<< HEAD
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
-// import Duolingo from './../../img/tabs/Duolingo.png';
-// import Flurry from './../../img/tabs/Flurry.png';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 
 storiesOf('Tabs', module)
   .addDecorator(withKnobs)
   .add('Default', () => (
-    <Tabs className="is-border">
-      <Tab tab="tab1">
-        <h1>tab1</h1>
-      </Tab>
-      <Tab tab="tab2">
-        <h1>tab2</h1>
-      </Tab>
-    </Tabs>
+    <Tabs
+      tabClassName={select(
+        'Type',
+        {
+          default: 'tab-list is-bg',
+          withBorder: 'tab-list is-border',
+          withShadow: 'tab-list is-shadow',
+          isCenter: 'tab-list is-center',
+        },
+        'tab-list is-bg'
+      )}
+    />
+  ))
+
+  .add('with border', () => (
+    <Tabs
+      tabClassName={select(
+        'Type',
+        {
+          default: 'tab-list is-bg',
+          withBorder: 'tab-list is-border',
+          withShadow: 'tab-list is-shadow',
+          isCenter: 'tab-list is-center',
+        },
+        'tab-list is-border'
+      )}
+    />
+  ))
+
+  .add('with shadow', () => (
+    <Tabs
+      tabClassName={select(
+        'Type',
+        {
+          default: 'tab-list is-bg',
+          withBorder: 'tab-list is-border',
+          withShadow: 'tab-list is-shadow',
+          isCenter: 'tab-list is-center',
+        },
+        'tab-list is-shadow'
+      )}
+    />
+  ))
+
+  .add('Is-Center', () => (
+    <Tabs
+      tabClassName={select(
+        'Type',
+        {
+          default: 'tab-list is-bg',
+          withBorder: 'tab-list is-border',
+          withShadow: 'tab-list is-shadow',
+          isCenter: 'tab-list is-center',
+        },
+        'tab-list is-center'
+      )}
+    />
+  ))
+
+  .add('Is-End', () => (
+    <Tabs
+      tabClassName={select(
+        'Type',
+        {
+          default: 'tab-list is-bg',
+          withBorder: 'tab-list is-border',
+          withShadow: 'tab-list is-shadow',
+          isCenter: 'tab-list is-center',
+        },
+        'tab-list is-end'
+      )}
+    />
   ));
-
-//   .add('with border', () => (
-//     <div class="custom-tab">
-//       <ul class="tab-list is-border">
-//           <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-//           <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-//           <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-//       </ul>
-//       <div class="custom-tab-content">
-//           <div class="tab-item" id="tab1">
-//               <ul class="tab-list">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-//           <div class="tab-item" id="tab2">
-//               <ul class="tab-list">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-//           <div class="tab-item current" id="tab2">
-//               <ul class="tab-list">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-
-//       </div>
-//   </div>
-//   ))
-
-//   .add('with shadow', () => (
-//     <div class="custom-tab">
-//       <ul class="tab-list is-shadow">
-//           <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-//           <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-//           <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-//       </ul>
-//       <div class="custom-tab-content">
-//           <div class="tab-item" id="tab1">
-//               <ul class="tab-list">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-//           <div class="tab-item" id="tab2">
-//               <ul class="tab-list">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-//           <div class="tab-item current" id="tab2">
-//               <ul class="tab-list">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-
-//       </div>
-//   </div>
-//   ))
-
-//   .add('Is-Center', () => (
-//     <div class="custom-tab">
-//       <ul class="tab-list is-bg is-center">
-//           <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-//           <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-//           <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-//       </ul>
-//       <div class="custom-tab-content">
-//           <div class="tab-item" id="tab1">
-//               <ul class="tab-list is-center">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-//           <div class="tab-item" id="tab2">
-//               <ul class="tab-list is-center">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-//           <div class="tab-item current" id="tab2">
-//               <ul class="tab-list is-center">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-
-//       </div>
-//   </div>
-//   ))
-
-//   .add('Is-End', () => (
-//     <div class="custom-tab">
-//       <ul class="tab-list is-bg is-end">
-//           <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-//           <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-//           <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-//       </ul>
-//       <div class="custom-tab-content">
-//           <div class="tab-item" id="tab1">
-//               <ul class="tab-list is-end">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-//           <div class="tab-item" id="tab2">
-//               <ul class="tab-list is-end">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-//           <div class="tab-item current" id="tab2">
-//               <ul class="tab-list is-end">
-//                   <li>
-//                       <a href="#">
-//                           <img src={Duolingo} alt="client"/>
-//                       </a>
-//                   </li>
-//                   <li>
-//                       <a href="#">
-//                           <img src={Flurry} alt="client"/>
-//                       </a>
-//                   </li>
-//               </ul>
-//           </div>
-
-//       </div>
-//   </div>
-//   ))
-=======
-import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
-
-storiesOf('Tabs', module)
-    .addDecorator(withKnobs)
-    .add('Default', () => (
-        <Tabs
-        tabClassName={select(
-                "Type",
-                {
-                    default: "tab-list is-bg",
-                    withBorder: "tab-list is-border",
-                    withShadow: "tab-list is-shadow",
-                    isCenter: "tab-list is-center"
-                },
-                "tab-list is-bg"
-            )}
-        />
-    ))
-
-    .add('with border', () => (
-        <Tabs
-        tabClassName={select(
-            "Type",
-            {
-                default: "tab-list is-bg",
-                withBorder: "tab-list is-border",
-                withShadow: "tab-list is-shadow",
-                isCenter: "tab-list is-center"
-            },
-            "tab-list is-border"
-        )}
-    />
-       
-    ))
-
-    .add('with shadow', () => (
-        <Tabs
-        tabClassName={select(
-            "Type",
-            {
-                default: "tab-list is-bg",
-                withBorder: "tab-list is-border",
-                withShadow: "tab-list is-shadow",
-                isCenter: "tab-list is-center"
-            },
-            "tab-list is-shadow"
-        )}
-    />
-    ))
-
-    .add('Is-Center', () => (
-        <Tabs
-        tabClassName={select(
-            "Type",
-            {
-                default: "tab-list is-bg",
-                withBorder: "tab-list is-border",
-                withShadow: "tab-list is-shadow",
-                isCenter: "tab-list is-center"
-            },
-            "tab-list is-center"
-        )}
-    />
-    ))
-
-    .add('Is-End', () => (
-        <Tabs
-        tabClassName={select(
-            "Type",
-            {
-                default: "tab-list is-bg",
-                withBorder: "tab-list is-border",
-                withShadow: "tab-list is-shadow",
-                isCenter: "tab-list is-center"
-            },
-            "tab-list is-end"
-        )}
-    />
-    ))
->>>>>>> a927914cb247d4067616864c033478c6d55be8e0
