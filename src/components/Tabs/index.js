dc98bafcec1e8e2f8fb3ea8d1ffd721d9fb41f6b
srthapa <srthapa_611@yahoo.com>
2021-01-12 20:54:57 +0545

    Update select component

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,9 +1,89 @@
-import React from 'react';
+import React, { Component, Children } from 'react';
+import PropTypes from 'prop-types';
 import './tabs.scss';
-import './../List/list.scss';
 
-const Tabs = (props) => (
-    <div className={`custom-tab ${props.className}`} onClick={props.onClick}>
-    </div>
-  );
-  export default Tabs;
+function Tab({ tab, children }) {
+  return <div tab={tab}>{children}</div>;
+}
+
+class Tabs extends Component {
+  constructor(props) {
+    super(props);
+    const {
+      props: { children },
+    } = this;
+    this.state = {
+      activeTab:
+        children && Children.count(children) === 1
+          ? children.props.tab
+          : children[0].props.tab,
+    };
+  }
+
+  onClickTabItem = (tab) => {
+    const { getActiveTab } = this.props;
+    this.setState({ activeTab: tab });
+    getActiveTab(tab);
+  };
+
+  render() {
+    const {
+      onClickTabItem,
+      props: { children, className },
+      state: { activeTab },
+    } = this;
+
+    return (
+      <div className="custom-tab">
+        <ul className={`tab-list is-bg ${className}`}>
+          {Children.map(children, (child) => {
+            const { tab } = child.props;
+            return (
+              <li key={tab} className={activeTab === tab ? 'current' : ''}>
+                {/* eslint-disable-next-line */}
+                <a
+                  onClick={() => onClickTabItem(tab)}
+                  onKeyDown={() => onClickTabItem(tab)}
+                  role="button"
+                  tabIndex={0}
+                  style={{ outline: 0 }}
+                >
+                  {tab}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="custom-tab-content">
+          {Children.map(children, (child) => {
+            if (child.props.tab !== activeTab) return null;
+            return child.props.children;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+Tab.propTypes = {
+  tab: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Tabs.defaultProps = {
+  header: null,
+  className: '',
+  footer: null,
+  getActiveTab: () => {},
+};
+
+Tabs.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  header: PropTypes.node,
+  getActiveTab: PropTypes.func,
+  footer: PropTypes.node,
+};
+
+export { Tab };
+export default Tabs;
--- src/components/Tabs/tab.stories.js
+++ src/components/Tabs/tab.stories.js
@@ -1,284 +1,240 @@
 import React from 'react';
-import Tabs from './index.js';
+import Tabs, { Tab } from './index.js';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
-import Duolingo from './../../img/tabs/Duolingo.png';
-import Flurry from './../../img/tabs/Flurry.png';
+// import Duolingo from './../../img/tabs/Duolingo.png';
+// import Flurry from './../../img/tabs/Flurry.png';
 
 storiesOf('Tabs', module)
   .addDecorator(withKnobs)
   .add('Default', () => (
-    <div class="custom-tab">
-      <ul class="tab-list is-bg">
-          <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-          <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-          <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-      </ul>
-      <div class="custom-tab-content">
-          <div class="tab-item" id="tab1">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item" id="tab2">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item current" id="tab2">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          
-      </div>
-  </div>
-  ))
+    <Tabs className="is-border">
+      <Tab tab="tab1">
+        <h1>tab1</h1>
+      </Tab>
+      <Tab tab="tab2">
+        <h1>tab2</h1>
+      </Tab>
+    </Tabs>
+  ));
 
-  .add('with border', () => (
-    <div class="custom-tab">
-      <ul class="tab-list is-border">
-          <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-          <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-          <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-      </ul>
-      <div class="custom-tab-content">
-          <div class="tab-item" id="tab1">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item" id="tab2">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item current" id="tab2">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          
-      </div>
-  </div>
-  ))
+//   .add('with border', () => (
+//     <div class="custom-tab">
+//       <ul class="tab-list is-border">
+//           <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
+//           <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
+//           <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
+//       </ul>
+//       <div class="custom-tab-content">
+//           <div class="tab-item" id="tab1">
+//               <ul class="tab-list">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+//           <div class="tab-item" id="tab2">
+//               <ul class="tab-list">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+//           <div class="tab-item current" id="tab2">
+//               <ul class="tab-list">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
 
-  .add('with shadow', () => (
-    <div class="custom-tab">
-      <ul class="tab-list is-shadow">
-          <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-          <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-          <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-      </ul>
-      <div class="custom-tab-content">
-          <div class="tab-item" id="tab1">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item" id="tab2">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item current" id="tab2">
-              <ul class="tab-list">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          
-      </div>
-  </div>
-  ))
+//       </div>
+//   </div>
+//   ))
 
-  .add('Is-Center', () => (
-    <div class="custom-tab">
-      <ul class="tab-list is-bg is-center">
-          <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-          <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-          <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-      </ul>
-      <div class="custom-tab-content">
-          <div class="tab-item" id="tab1">
-              <ul class="tab-list is-center">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item" id="tab2">
-              <ul class="tab-list is-center">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item current" id="tab2">
-              <ul class="tab-list is-center">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          
-      </div>
-  </div>
-  ))
+//   .add('with shadow', () => (
+//     <div class="custom-tab">
+//       <ul class="tab-list is-shadow">
+//           <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
+//           <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
+//           <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
+//       </ul>
+//       <div class="custom-tab-content">
+//           <div class="tab-item" id="tab1">
+//               <ul class="tab-list">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+//           <div class="tab-item" id="tab2">
+//               <ul class="tab-list">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+//           <div class="tab-item current" id="tab2">
+//               <ul class="tab-list">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
 
-  .add('Is-End', () => (
-    <div class="custom-tab">
-      <ul class="tab-list is-bg is-end">
-          <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
-          <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
-          <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
-      </ul>
-      <div class="custom-tab-content">
-          <div class="tab-item" id="tab1">
-              <ul class="tab-list is-end">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item" id="tab2">
-              <ul class="tab-list is-end">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="tab-item current" id="tab2">
-              <ul class="tab-list is-end">
-                  <li>
-                      <a href="#">
-                          <img src={Duolingo} alt="client"/>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#">
-                          <img src={Flurry} alt="client"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          
-      </div>
-  </div>
-  ))
+//       </div>
+//   </div>
+//   ))
+
+//   .add('Is-Center', () => (
+//     <div class="custom-tab">
+//       <ul class="tab-list is-bg is-center">
+//           <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
+//           <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
+//           <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
+//       </ul>
+//       <div class="custom-tab-content">
+//           <div class="tab-item" id="tab1">
+//               <ul class="tab-list is-center">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+//           <div class="tab-item" id="tab2">
+//               <ul class="tab-list is-center">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+//           <div class="tab-item current" id="tab2">
+//               <ul class="tab-list is-center">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+
+//       </div>
+//   </div>
+//   ))
+
+//   .add('Is-End', () => (
+//     <div class="custom-tab">
+//       <ul class="tab-list is-bg is-end">
+//           <li class=""><a href="#"  data-tab="tab1">Test 1</a></li>
+//           <li class=""><a href="#" data-tab="tab2">Test 2</a></li>
+//           <li class="current"><a href="#" data-tab="tab3">Test 3</a></li>
+//       </ul>
+//       <div class="custom-tab-content">
+//           <div class="tab-item" id="tab1">
+//               <ul class="tab-list is-end">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+//           <div class="tab-item" id="tab2">
+//               <ul class="tab-list is-end">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+//           <div class="tab-item current" id="tab2">
+//               <ul class="tab-list is-end">
+//                   <li>
+//                       <a href="#">
+//                           <img src={Duolingo} alt="client"/>
+//                       </a>
+//                   </li>
+//                   <li>
+//                       <a href="#">
+//                           <img src={Flurry} alt="client"/>
+//                       </a>
+//                   </li>
+//               </ul>
+//           </div>
+
+//       </div>
+//   </div>
+//   ))
